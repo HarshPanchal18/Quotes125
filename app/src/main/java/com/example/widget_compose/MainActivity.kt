d4e8d7a12003157ddce37087bcbd70245978f2b3
Harsh Panchal <harshhhh1803@gmail.com>
2023-09-29 14:11:09 +0530

    Added Floating Action Button for scrolling back to the top of quote list, Manipulated UI part with @Composable functions Written total 125 quotes

--- app/src/main/java/com/example/widget_compose/MainActivity.kt
+++ app/src/main/java/com/example/widget_compose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.widget_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,14 +16,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -35,93 +49,142 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.widget_compose.repository.QuoteRepository
 import com.example.widget_compose.ui.theme.WidgetComposeTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
 
     private val repo = QuoteRepository()
+    private lateinit var listState: LazyListState
+    private lateinit var coroutineScope: CoroutineScope
+    //private var isScrollingDown by mutableStateOf(false)
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             WidgetComposeTheme {
-                Scaffold(
-                    topBar = { AppBar() }
-                ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(it)
-                    ) {
-                        LazyColumn {
-                            itemsIndexed(
-                                repo.getQuoteList()
-                            ) { index, currentQuote ->
-                                QuoteCard(currentQuote, index + 1)
-                            }
-                        }
+                HomeScreen()
+            }
+        }
+    }
+
+    @Composable
+    fun QuoteCard(currentQuote: String, index: Int) {
+        Card(
+            modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            elevation = CardDefaults.cardElevation(4.dp)
+        ) {
+            Box(
+                modifier = Modifier,
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Text(
+                    text = currentQuote,
+                    color = Color.Black,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    fontFamily = FontFamily.Monospace
+                )
+                Text(
+                    text = "$index",
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .padding(end = 12.dp),
+                    fontWeight = FontWeight.Light,
+                    fontSize = 35.sp,
+                    color = Color.Gray.copy(0.6F)
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun AppBar() {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "",
+                modifier = Modifier
+                    .padding(start = 8.dp)
+                    .clip(RoundedCornerShape(24.dp))
+                    .size(75.dp)
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = stringResource(R.string.app_name),
+                fontSize = 20.sp,
+                fontFamily = FontFamily.Monospace,
+                modifier = Modifier.weight(1F)
+            )
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun HomeScreen() {
+        listState = rememberLazyListState()
+        coroutineScope = rememberCoroutineScope()
+
+        Scaffold(
+            topBar = { AppBar() },
+            floatingActionButton = {
+                BackToTopFAB {
+                    coroutineScope.launch { listState.animateScrollToItem(index = 0) }
+                }
+            }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(it)
+            ) {
+                LazyColumn(state = listState) {
+                    itemsIndexed(
+                        repo.getQuoteList()
+                    ) { index, currentQuote ->
+                        QuoteCard(currentQuote, index + 1)
                     }
                 }
             }
         }
     }
-}
 
-@Composable
-fun QuoteCard(currentQuote: String, index: Int) {
-    Card(
-        modifier = Modifier
-            .padding(4.dp)
-            .fillMaxWidth(),
-        elevation = CardDefaults.cardElevation(4.dp)
-    ) {
-        Box(
-            modifier = Modifier,
-            contentAlignment = Alignment.CenterEnd
+    @Composable
+    fun BackToTopFAB(onClick: () -> Unit) {
+        FloatingActionButton(
+            onClick = { onClick() },
+            containerColor = Color(0xFFFFEB3B)
         ) {
-            Text(
-                text = currentQuote,
-                color = Color.Black,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                fontFamily = FontFamily.Monospace
-            )
-            Text(
-                text = "$index",
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .padding(end = 12.dp),
-                fontWeight = FontWeight.Light,
-                fontSize = 35.sp,
-                color = Color.Gray.copy(0.6F)
+            Icon(
+                imageVector = Icons.Default.KeyboardArrowUp,
+                contentDescription = "Back to top"
             )
         }
     }
-}
 
-@Composable
-fun AppBar() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            painter = painterResource(R.drawable.logo),
-            contentDescription = "",
-            modifier = Modifier
-                .padding(start = 8.dp)
-                .clip(RoundedCornerShape(24.dp))
-                .size(75.dp)
-        )
-        Spacer(modifier = Modifier.width(20.dp))
-        Text(
-            text = stringResource(R.string.app_name),
-            fontSize = 20.sp,
-            fontFamily = FontFamily.Monospace,
-            modifier = Modifier.weight(1F)
-        )
-    }
+    /*@Composable
+    fun LazyListState.isScrollingDown(): Boolean {
+        var prevIndex by remember(this) { mutableStateOf(firstVisibleItemIndex) }
+        var prevScrollOffset by remember(this) { mutableStateOf(firstVisibleItemScrollOffset) }
+        return remember(this) {
+            derivedStateOf {
+                if (prevIndex != firstVisibleItemIndex) {
+                    prevIndex > firstVisibleItemIndex
+                } else {
+                    prevScrollOffset >= firstVisibleItemScrollOffset
+                }.also {
+                    prevIndex = firstVisibleItemIndex
+                    prevScrollOffset = firstVisibleItemScrollOffset
+                }
+            }.value
+        }
+    }*/
 }
